68a009265a8b05b7b5b9ee339fad5cd0fc7bcb78
Muhammad <azhar40@live.co.uk>
2020-12-07 23:18:15 +0500

    classmedekhenge

--- app.js
+++ app.js
@@ -1,51 +1,52 @@
-var userPasswords = ["haha"]
-var userEmail = ["azharpathan@gmail.com"];
+var userPasswords = ["azhar"]
+var userEmail = ["azharkhan@gmail.com"];
+var usersArr = ["azhar"];
 var currentUsername = "";
 
 
 const signup = () => {
 
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
+    var user = document.getElementById("name").value;
+    var email = document.getElementById("email").value.toLowerCase();
     var password = document.getElementById("password").value;
-    var male = document.getElementById("male").value;
-    var female = document.getElementById("female").value;
+    // var male = document.getElementById("male").value;
+    // var female = document.getElementById("female").value;
 
     if (userEmail.indexOf(email) === -1) {
-        currentUsername += name;
         userEmail.push(email);
         userPasswords.push(password);
-        console.log(userEmail);
-        console.log(userPasswords);
-        window.location.href="login.html";
+        usersArr.push(user);
+        console.log(usersArr);
+        localStorage.setItem("userPasswords", JSON.stringify(userPasswords));
+        localStorage.setItem("userEmail", JSON.stringify(userEmail));
+        window.location.href = "login.html"
     }
 
     else {
-     alert("email already exists");
+        alert("email already exists");
     }
     return false;
-
-
 }
 
+   
+
+
 const check = () => {
-    for (let i = 0; i < userEmail.length; i++) {
-        userEmail[i] = userEmail[i].toLowerCase()
-        console.log("current user===>", userEmail[i]);
-    }
+
 
     var passsword = document.getElementById("password").value;
     var email = document.getElementById("email").value;
 
- if (userPasswords.indexOf(passsword)===-1 || userEmail.indexOf(email)===-1)
- {
-    alert("Wrong Password or email");
-    console.log("ye wala chalrha hai");
- }
-
-        else {
-           alert("logged in succesfully")
-           window.location.href = "index.html";
-        }
-        return false;
+    if (userPasswords.indexOf(passsword) === -1 || userEmail.indexOf(email) === -1) {
+        alert("Wrong Password or email");
+        console.log("ye wala chalrha hai");
+        var i = indexOf(password);
+        console.log(i);
     }
+    else {
+        alert("logged in succesfully")
+        window.location.href = "index.html";
+    }
+    return false;
+}
+
